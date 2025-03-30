678e6ed43ff3eb04b218a28478a06ab21cdf2a7f
mohankopuru <64723262+mohankopuru@users.noreply.github.com>
2025-03-30 13:37:39 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,11 @@
-
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  // Use '/' for root domain deployment
-  base: '/',
+export default defineConfig({
+  plugins: [react()],
+  base: '/', // Setting base to root for GitHub Pages
   server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-}));
+    port: 8080
+  }
+});
